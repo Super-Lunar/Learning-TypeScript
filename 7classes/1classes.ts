82435c2632c8ab9225753559a07fbc8981739af5
sharmasaurabh4476@gmail.com <sharmasaurabh4476@gmail.com>
2024-10-28 12:32:42 +0545

    classes and inheritance in tsc

---
+++ 7classes/1classes.ts
@@ -0,0 +1,32 @@
+export {};
+
+class Employee {
+  employeeName: string;
+  constructor(name: string) {
+    this.employeeName = name;
+  }
+  showName() {
+    console.log(`Employee Name : ${this.employeeName}`);
+  }
+}
+
+let emp1 = new Employee("Beast");
+console.log(emp1.employeeName);
+emp1.showName();
+
+// for inheritance use extends
+
+class Manager extends Employee {
+  constructor(managerName: string) {
+    super(managerName);
+  }
+  delegateWork() {
+    console.log("Manager delegating tasks");
+  }
+}
+
+let m1 = new Manager("Bruce");
+
+m1.delegateWork();
+m1.showName();
+console.log(m1.employeeName);
